9500eec64ee69e920d759335236da90cf36b7955
Utkarsh Deep <utkarshdeep@outlook.com>
2019-04-10 16:13:58 +0530

    environment variables introduced for configs

--- config.js
+++ config.js
@@ -1,6 +1,11 @@
+const { PORT, REPORTS_PATH, PUBLIC_PATH, MONGO_HOST, DBNAME } = process.env
+const port = PORT
+const reportsPath = REPORTS_PATH
+const publicPath = PUBLIC_PATH
+const mongoUrl = `mongodb://${MONGO_HOST}/${DBNAME}`
 module.exports = {
-  port: 3000,
-  reportsPath: '/public/reports/',
-  publicPath: '/public',
-  mongoUrl: 'mongodb://localhost:27017/KitchenDisplaySystem'
+  port,
+  reportsPath,
+  publicPath,
+  mongoUrl
 }
